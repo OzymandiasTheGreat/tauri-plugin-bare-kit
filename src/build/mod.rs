d473e9d849745155acd2687f47dc37f902f205fe
Tomas Ravinskas <tomas@tomasrav.me>
2026-04-25 17:28:36 +0300

    Re-run if bare js sources change

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -1,6 +1,7 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 pub fn autolink() {
+    let project = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let resource_dir = env::var("DEP_TAURI_PLUGIN_BARE_KIT_RESOURCE_DIR").unwrap();
 
     env::set_var(
@@ -8,6 +9,34 @@ pub fn autolink() {
         format!("{{ \"bundle\": {{ \"resources\": {{ \"{resource_dir}\": \"\" }} }} }}"),
     );
 
+    println!(
+        "cargo::rerun-if-changed={}",
+        project.parent().unwrap().join("bare").display()
+    );
+    println!(
+        "cargo::rerun-if-changed={}",
+        project.parent().unwrap().join("src-bare").display()
+    );
+    println!(
+        "cargo::rerun-if-changed={}",
+        project.parent().unwrap().join("common").display()
+    );
+    println!(
+        "cargo::rerun-if-changed={}",
+        project.parent().unwrap().join("src-common").display()
+    );
+    println!(
+        "cargo::rerun-if-changed={}",
+        project.parent().unwrap().join("app.bundle.json").display()
+    );
+    println!(
+        "cargo::rerun-if-changed={}",
+        project
+            .parent()
+            .unwrap()
+            .join("app.bundle.json.d")
+            .display()
+    );
     println!("cargo::rustc-link-arg=-Wl,-rpath,@executable_path/");
     println!("cargo::rustc-link-search=framework={resource_dir}");
     println!("cargo::rustc-link-lib=framework=BareKit");
