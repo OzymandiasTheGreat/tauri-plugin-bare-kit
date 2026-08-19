914afb5b766b2bb050efbd63efdc4ad1bd6a4a2c
Tomas Ravinskas <tomas@tomasrav.me>
2026-08-19 21:32:12 +0300

    - Use old method to kill appium
    Signed-off-by: Tomas Ravinskas <tomas@tomasrav.me>

--- tests/test-android.sh
+++ tests/test-android.sh
@@ -2,5 +2,7 @@ set -euo pipefail
 
 npx appium --allow-insecure "*:chromedriver_autodownload" &
 APPIUM_PID=$!
-trap 'kill "$APPIUM_PID" 2>/dev/null || true' EXIT
 npm run test:android
+EXIT_CODE=$?
+kill $APPIUM_PID
+exit $EXIT_CODE
