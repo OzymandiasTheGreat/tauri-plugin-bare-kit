aa91ffd801f4fc2f9df91f71994c6879672c0f3f
Tomas Ravinskas <tomas@tomasrav.me>
2026-08-20 17:42:45 +0300

    - CI: Also kill crashpad_handler
    Signed-off-by: Tomas Ravinskas <tomas@tomasrav.me>

--- tests/test-android.sh
+++ tests/test-android.sh
@@ -7,13 +7,10 @@ APPIUM_PID=$!
 cleanup() {
     echo "Stopping Appium (PID $APPIUM_PID)..."
     kill "$APPIUM_PID" 2>/dev/null || true
-    wait "$APPIUM_PID" 2>/dev/null || true
+    pkill -TERM -f '/android/sdk/emulator/crashpad_handler' || true
+    ps -ef | grep -E '[a]ppium|[c]hromedriver|[c]rashpad|[e]mulator' || true
 }
 
 trap cleanup EXIT
 
 npm run test:android
-
-echo "TESTS FINISHED"
-echo "Appium PID: $APPIUM_PID"
-ps -ef | grep -E '[a]ppium|[c]hromedriver|[c]rashpad_handler'
