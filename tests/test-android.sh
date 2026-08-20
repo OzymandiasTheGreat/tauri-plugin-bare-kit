32b806eb5491d0a613c205f3d54e550dd979715a
Tomas Ravinskas <tomas@tomasrav.me>
2026-08-20 19:55:33 +0300

    - CI: Merge workflows
    Signed-off-by: Tomas Ravinskas <tomas@tomasrav.me>

--- tests/test-android.sh
+++ tests/test-android.sh
@@ -5,11 +5,8 @@ setsid npx appium --allow-insecure "*:chromedriver_autodownload" &
 APPIUM_PID=$!
 
 cleanup() {
-    echo "Stopping Appium (PID $APPIUM_PID)..."
     kill -- "-$APPIUM_PID" 2>/dev/null || true
     wait "$APPIUM_PID" 2>/dev/null || true
-    # pkill -TERM -f '/android/sdk/emulator/crashpad_handler' || true
-    ps -ef | grep -E '[a]ppium|[c]hromedriver|[c]rashpad|[e]mulator' || true
 }
 
 trap cleanup EXIT
